1e0b57b911fdd22c1084585442eb09482e5b694e
Kushal Pisavadia <kushi.p@gmail.com>
2013-05-12 18:10:42 +0100

    Put the logs-matching fn back
    This didn't need to be deleted in the first place. It wasn't trying to
    rebind anything and just puts the matches back into a form that tests
    can parse.

--- test/com/puppetlabs/testutils/logging.clj
+++ test/com/puppetlabs/testutils/logging.clj
@@ -1,1 +1,23 @@
 (ns com.puppetlabs.testutils.logging)
+
+(defn- log-entry->map
+  [log-entry]
+  {:namespace (get log-entry 0)
+   :level     (get log-entry 1)
+   :exception (get log-entry 2)
+   :message   (get log-entry 3)})
+
+(defn logs-matching
+  "Given a regular expression pattern and a sequence of log messages (in the format
+  used by `clojure.tools.logging`, return only the logs whose message matches the
+  specified regular expression pattern.  (Intended to be used alongside
+  `with-log-output` for tests that are validating log output.)  The result is
+  a sequence of maps, each of which contains the following keys:
+  `:namespace`, `:level`, `:exception`, and `:message`."
+  [pattern logs]
+  {:pre  [(instance? java.util.regex.Pattern pattern)
+          (coll? logs)]}
+  ;; the logs are formatted as sequences, where the string at index 3 contains
+  ;; the actual log message.
+  (let [matches (filter #(re-find pattern (get % 3)) logs)]
+    (map log-entry->map matches)))
