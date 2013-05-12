4a4a78dfeecbc51beb40cb32bdfa38e22ff2e0b0
Kushal Pisavadia <kushi.p@gmail.com>
2013-05-12 19:41:18 +0100

    Add atom-logger and with-log-output functions back
    These helper functions have been updated to work with timbre. Also
    provided some (basic) tests to make it easier for others to refactor
    these in the future.

--- test/com/puppetlabs/testutils/logging.clj
+++ test/com/puppetlabs/testutils/logging.clj
@@ -1,11 +1,11 @@
-(ns com.puppetlabs.testutils.logging)
+(ns com.puppetlabs.testutils.logging
+  (:use [taoensso.timbre :as timbre]))
 
 (defn- log-entry->map
   [log-entry]
   {:namespace (get log-entry 0)
    :level     (get log-entry 1)
-   :exception (get log-entry 2)
-   :message   (get log-entry 3)})
+   :message   (get log-entry 2)})
 
 (defn logs-matching
   "Given a regular expression pattern and a sequence of log messages (in the format
@@ -19,5 +19,38 @@
           (coll? logs)]}
   ;; the logs are formatted as sequences, where the string at index 3 contains
   ;; the actual log message.
-  (let [matches (filter #(re-find pattern (get % 3)) logs)]
+  (let [matches (filter #(re-find pattern (get % 2)) logs)]
     (map log-entry->map matches)))
+
+(defn atom-logger [output-atom]
+  "Send logs to the supplied atom"
+  (timbre/set-config!
+   [:appenders :puppetdb-test-appender]
+   {:doc       "PuppetDB Test Appender"
+    :min-level :debug
+    :enabled?  true
+    :async?    false
+    :max-message-per-msecs nil ; No rate limiting
+    :fn (fn [{:keys [ap-config level prefix message more] :as args}]
+          (when-not (:my-production-mode? ap-config)
+            (swap! output-atom conj [prefix level message more])))}))
+
+(defmacro with-log-output
+  "Sets up a temporary logger to capture all log output to a sequence, and
+  evaluates `body` in this logging context.
+
+  `log-output-var` - Inside of `body`, the variable named `log-output-var`
+  is a clojure atom containing the sequence of log messages that have been logged
+  so far.  You can access the individual log messages by dereferencing this
+  variable (with either `deref` or `@`).
+
+  Example:
+
+      (with-log-output logs
+        (log/info \"Hello There\")
+        (is (= 1 (num-logs-matching #\"Hello There\" @logs))))
+  "
+  [log-output-var & body]
+  `(let [~log-output-var (atom [])]
+     (do (atom-logger ~log-output-var)
+         ~@body)))
--- test/com/puppetlabs/testutils/logging_test.clj
+++ test/com/puppetlabs/testutils/logging_test.clj
@@ -1,6 +1,7 @@
 (ns com.puppetlabs.testutils.logging-test
   (:use [clojure.test :only (deftest testing is)]
-        [com.puppetlabs.testutils.logging]))
+        [com.puppetlabs.testutils.logging]
+        [taoensso.timbre :as log]))
 
 (deftest test-logs-matching
   (testing "throws an exception when expected inputs aren't provided"
@@ -8,6 +9,21 @@
     (is (thrown? AssertionError (logs-matching 1 2)))
     (is (thrown? AssertionError (logs-matching "ab" ["a" "b"]))))
   (testing "extracts the log messages that match the output we provide"
-    (is (= '({:namespace :a, :level :b, :exception :c, :message "abc"})
-           (logs-matching #"ab" [[:a :b :c "abc"]
-                                 [:x :y :z "qe"]])))))
+    (is (= '({:namespace :time, :level :info, :message "abc"})
+           (logs-matching #"ab" [[:time :info "abc"]
+                                 [:time :info "qe"]])))))
+
+(deftest test-atom-logger
+  (testing "pipes any log output to the provided atom"
+    (let [logs (atom [])]
+      (do
+        (atom-logger logs)
+        (log/info "test"))
+      (is (= "test" (-> @logs (first) (get 2)))))))
+
+(deftest test-with-log-output
+  (testing "pipes log output to the provided atom"
+    (let [logs (atom [])]
+      (with-log-output logs
+        (log/info "Hello")
+        (is (= "Hello" (-> @logs (first) (get 2))))))))
