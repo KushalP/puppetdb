f5da4dfdb8cc46530dc181abd008eb734cde7afb
Kushal Pisavadia <kushi.p@gmail.com>
2013-11-17 22:36:06 +0000

    Save a Fact record to the database
    Given a Fact record of the form (Fact. name value timestamp certname)
    saves it to the database when the (save! ...) function is called on
    the record.
    
    This will provide the abstraction required for splitting out the
    'command' layer and the 'query' layer, but still providing a
    nicer (interfaced) data structure to play with on either side.

--- src/com/puppetlabs/puppetdb/scf/records.clj
+++ src/com/puppetlabs/puppetdb/scf/records.clj
@@ -1,4 +1,13 @@
-(ns com.puppetlabs.puppetdb.scf.records)
+(ns com.puppetlabs.puppetdb.scf.records
+  (:require [clojure.java.jdbc :as sql]))
 
-(defrecord Fact [name value timestamp certname])
+(defprotocol Commandable
+  (save! [this]))
 
+(defrecord Fact [name value timestamp certname]
+  Commandable
+  (save! [this]
+    (let [fact-id-map (sql/insert-record :facts (select-keys this [:name :value]))]
+      (->> (select-keys this [:timestamp :certname])
+           (merge fact-id-map)
+           (sql/insert-record :facts_metadata)))))
--- test/com/puppetlabs/puppetdb/test/scf/records.clj
+++ test/com/puppetlabs/puppetdb/test/scf/records.clj
@@ -1,13 +1,35 @@
 (ns com.puppetlabs.puppetdb.test.scf.records
-  (:use [clojure.test])
-  (:require [com.puppetlabs.puppetdb.scf.records])
+  (:use [clj-time.coerce :only [to-timestamp]]
+        [clj-time.core :only [now]]
+        [clojure.test]
+        [com.puppetlabs.jdbc :only [query-to-vec]]
+        [com.puppetlabs.puppetdb.fixtures]
+        [com.puppetlabs.puppetdb.scf.storage :only [add-certname!]])
+  (:require [com.puppetlabs.puppetdb.scf.records :refer :all])
   (:import [com.puppetlabs.puppetdb.scf.records Fact]))
 
+(use-fixtures :each with-test-db)
+
 (deftest test-facts
-  (testing "it has the relevant queryable keys"
-    (let [fact (Fact. "kernel" "Linux" "some timestamp" "host.local")]
-      (are [field result] (= result (get-in fact [field]))
-           :name      "kernel"
-           :value     "Linux"
-           :timestamp "some timestamp"
-           :certname  "host.local"))))
+  (let [time (to-timestamp (now))
+        certname "some_certname.local"]
+    (add-certname! certname)
+    (testing "it has the relevant queryable keys"
+      (let [fact (Fact. "kernel" "Linux" time certname)]
+        (are [field result] (= result (get-in fact [field]))
+             :name      "kernel"
+             :value     "Linux"
+             :timestamp time
+             :certname  certname)))
+    (testing "protocols"
+      (testing "can successfully save a fact to the database"
+        (let [fact (Fact. "kernel" "Linux" time certname)]
+          (save! fact)
+          (is (= (query-to-vec (str "SELECT f.name, f.value, m.certname, m.timestamp "
+                                    "FROM facts AS f "
+                                    "INNER JOIN facts_metadata AS m "
+                                    "ON f.fact_id = m.fact_id"))
+                 [{:name      "kernel"
+                   :value     "Linux"
+                   :timestamp time
+                   :certname  certname}])))))))
